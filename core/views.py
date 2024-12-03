52b84d0504ea0bd09d4c28b77520a4164d41f492
HaleymHidalgo <haleymgabrielhidalgo@gmail.com>
2024-12-03 18:26:42 -0300

    fix url

--- core/views.py
+++ core/views.py
@@ -2,8 +2,5 @@ from django.http import HttpResponse
 from django.shortcuts import render
 
 # Create your views here.
-def hello(request):
-    return HttpResponse("Hello, Django!")
-
 def index(request):
     return render(request, 'index.html')
